ddca3310c21f1bbb0b28cd1f3fa33dba96faf1f1
Arthur Pedroti <arthurpedroti@gmail.com>
2020-08-04 17:30:12 -0300

    feat: creating tickets migration and entity

---
+++ src/modules/tickets/infra/typeorm/entities/Ticket.ts
@@ -0,0 +1,49 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  JoinColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import User from '@modules/users/infra/typeorm/entities/User';
+
+@Entity('tickets')
+class Ticket {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  user_id: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'user_id' })
+  provider: User;
+
+  @Column()
+  client: string;
+
+  @Column()
+  classification: string;
+
+  @Column()
+  equipment: string;
+
+  @Column()
+  type: string;
+
+  @Column()
+  status: string;
+
+  @Column()
+  description: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default Ticket;
---
+++ src/shared/infra/typeorm/migrations/1596567549787-CreateTickets.ts
@@ -0,0 +1,83 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm';
+
+export default class CreateTickets1596567549787 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'tickets',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'user_id',
+            type: 'uuid',
+            isNullable: true,
+          },
+          {
+            name: 'client',
+            type: 'varchar',
+          },
+          {
+            name: 'classification',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'equipment',
+            type: 'varchar',
+          },
+          {
+            name: 'type',
+            type: 'varchar',
+          },
+          {
+            name: 'status',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'description',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'tickets',
+      new TableForeignKey({
+        name: 'OwnerUser',
+        columnNames: ['user_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'users',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('tickets', 'OwnerUser');
+    await queryRunner.dropTable('tickets');
+  }
+}
