7ca7d0917d5572ff468c19eb8c9f2e810745451c
Arthur Pedroti <arthurpedroti@gmail.com>
2020-10-22 10:33:02 -0300

    test: test

--- src/modules/tickets/infra/http/controllers/ProductsController.ts
+++ src/modules/tickets/infra/http/controllers/ProductsController.ts
@@ -5,10 +5,10 @@ export default class ProductsController {
   public async index(request: Request, response: Response): Promise<Response> {
     const products = await axios.get(
       `${process.env.APP_PROTHEUS_API_URL}/products`,
-      {
-        headers: request.headers,
-        // params: request.params,
-      },
+      // {
+      //   headers: request.headers,
+      //   params: request.params,
+      // },
     );
 
     return response.json(products.data);
